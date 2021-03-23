160c6b3f9f14e0b6c305d8112dddff81c0548450
AdityaJsr <adityajsr1996@gmail.com>
2021-03-23 11:50:20 +0530

    ADDED : CONCAT

---
+++ week 4/mysql/functions.py
@@ -0,0 +1,71 @@
+import mysql.connector as msc
+
+mydb = msc.connect(
+    host = 'localhost' ,
+    user = 'root' ,
+    passwd = 'admin',
+    database = 'employeedb'
+    )
+
+
+mycursor = mydb.cursor()
+
+# mycursor.execute("""CREATE TABLE people (first_name VARCHAR(50) NOT NULL,
+#  last_name VARCHAR(50) NOT NULL, personID int PRIMARY KEY AUTO_INCREMENT)""")
+
+def addRecord():
+    try:
+        fName = input("Enter the First name : ").upper()
+        lName = input("Enter the Last name : ").upper()
+        mycursor.execute("INSERT INTO people (first_name,last_name) VALUES (%s,%s)",(fName , lName ))
+        mydb.commit()
+    except ValueError as ve:
+        print("Enter proper value : ",ve)
+    except Exception as e:
+        print("Oops something went wrong !!", e)
+    except:  
+        # rollback used for if any error   
+        mydb.rollback()  
+        mydb.close()
+    
+def showDb():
+    mycursor.execute("SELECT * from people")
+    for items in mycursor:
+        fName = items[0],
+        lName = items[1],
+        id = items[2]
+        print('First_Name = ', fName , 'Last_Name = ', lName , 'I.D = ', id )
+def createFun():
+    mycursor.execute("""
+        CREATE FUNCTION full_name(first_name CHAR(30),last_name CHAR(30))
+        RETURNS CHAR(65) DETERMINISTIC
+        RETURN CONCAT(first_name,' ',last_name)
+    """)
+
+def useCreateFun():
+
+    mycursor.execute("""
+        SELECT full_name(first_name,last_name) AS name
+        FROM people
+    """)
+
+    for x in mycursor:
+        print(x)
+
+def describeTable():
+
+    mycursor.execute("""
+        DESC people
+    """)
+    for items in mycursor:
+        print(items)
+
+def main():
+    # addRecord()
+    # showDb()
+    # createFun()
+    # useCreateFun()
+    # describeTable()
+
+if __name__ == "__main__":
+    main()
